b31db301cb9df73a406cb64dc62aa639817217b4
Tim Dwyer <tgdwyer>
2019-12-01 12:01:27 +1100

    cleanup

--- src/Lib.hs
+++ src/Lib.hs
@@ -4,11 +4,11 @@ module Lib
 , eightQueens
 ) where
 import Control.Monad
-import Data.List (tails)
+import Data.List (tails,unfoldr)
 
 type Board = [Int]
 
--- Check the first queen against all her sisters.
+-- | Check the first queen against all her sisters.
 check :: Board -> Bool
 check [] = True
 check (q:qs) = all ok $ zip [1..] qs
@@ -16,41 +16,40 @@ check (q:qs) = all ok $ zip [1..] qs
                         && vdist /= hdist -- not on a diagonal
                           where vdist = abs (q-q')
 
--- list monad version
-nQueensLM n = extend n n []
+-- | Build a set of nQueens solution boards by recursing inside the list monad.
+nQueens_ListMonad :: Int -> [Board]
+nQueens_ListMonad n = let
+    extend :: Int -> Board -> [Board]
+    extend 0 fullBoard = [fullBoard]
+    extend remaining partialBoard = do
+      -- Non-deterministically "choose" a value.
+      q <- [0..n-1]
+      -- Glue it on the front.
+      let extended = q:partialBoard
+      -- Make sure it's legal.
+      guard $ check extended
+      -- Recurse.
+      extend (remaining-1) extended
+  in
+    extend n []
 
-extend :: Int -> Int -> Board -> [Board]
-extend n 0 partial = return partial
-extend n remaining partial = do
-  -- Non-deterministically "choose" a value.
-  q <- [0..n-1]
-  -- Glue it on the front.
-  let extended = q:partial
-  -- Make sure it's legal.
-  guard $ check extended
-  -- Recurse.
-  extend n (remaining-1) extended
-
--- check a full board
+-- | check a full board
 checkBoard qs = all check $ tails qs
 
-nQueensLC n =
-    let
-        -- for a given set of columns,
-        -- prepend every column possible
-        addQueen :: Board -> [Board]
-        addQueen cols = filter check [r:cols | r <- [0..n-1]]
+-- | fold the concatMap of a function that uses a list comprehension to generate partial solutions
+nQueens_ListComprehension :: Int -> [Board]
+nQueens_ListComprehension n =
+  let
+    -- for a given partial board (set of columns),
+    -- prepend every column possible
+    addQueen :: Board -> [Board]
+    addQueen board = [boards | boards <- [r:board | r <- [0..n-1]], checkBoard boards]
+  in
+    foldr (const $ concatMap addQueen) [[]] [0..n-1]
 
-        -- generate k columns of feasible queens
-        queens :: Int -> [Board] -> [Board]
-        queens 0 s = s
-        queens k s = queens (k-1) (concatMap addQueen s)
-    in
-        queens n [[]]
-
-
+-- | create a list of solutions to nQueens for a given n
 nQueens :: Int -> [Board]
-nQueens = nQueensLC
+nQueens = nQueens_ListMonad
 
 eightQueens :: [Board]
 eightQueens = nQueens 8
